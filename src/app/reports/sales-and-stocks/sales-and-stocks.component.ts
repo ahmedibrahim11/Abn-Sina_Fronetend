4a42944754f62f832ab18f1fe93bd04f3ebf8497
Ahmed <ahmed.abdelkhalek1@vodafone.com>
2021-02-02 22:19:14 +0200

    change header functions

--- src/app/reports/sales-and-stocks/sales-and-stocks.component.ts
+++ src/app/reports/sales-and-stocks/sales-and-stocks.component.ts
@@ -7,85 +7,69 @@ import * as XLSX from 'xlsx';
   styleUrls: ['./sales-and-stocks.component.css']
 })
 export class SalesAndStocksComponent implements OnInit {
- 
 
-   url:string="Report/downloadFile";
 
-data:any[]=[];
-tableHeaders:any[]=[];
-  
-  constructor(private _fileService:FileService) { }
+  url: string = "Report/downloadFile";
+
+  data: any[] = [];
+  tableHeaders: any[] = [];
+
+  constructor(private _fileService: FileService) { }
 
   ngOnInit(): void {
-this.getExelfile();
+    this.getExelfile();
   }
 
-  getExelfile(){
+  getExelfile() {
 
-    this._fileService.downloadFile("Daily_Sales_and_Stock_for_AL_ANDALOUS20210110080017.xlsx",this.url).subscribe(
+    this._fileService.downloadFile("testExcel.xlsx", this.url).subscribe(
       {
-        next:(res)=>{
-        
+        next: (res) => {
+          debugger;
           this.extractDataFromExcel(res);
-
         },
-    
-        error:(err)=>{console.log(err)},
-    }
-      
-
-
+        error: (err) => { console.log(err) },
+      }
     )
   }
 
- 
-
-
-
-
   extractDataFromExcel(file: any) {
     /* wire up file reader */
-  
     const reader: FileReader = new FileReader();
     reader.readAsBinaryString(file);
     reader.onload = (e: any) => {
       /* create workbook */
       const binarystr: string = e.target.result;
       const wb: XLSX.WorkBook = XLSX.read(binarystr, { type: 'binary' });
-
+      debugger;
       /* selected the first sheet */
       const wsname: string = wb.SheetNames[0];
       const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-      this.getheaders(ws);
+      this.get_header_row(ws);
       /* save data */
-      this.data = XLSX.utils.sheet_to_json(ws); // to get 2d array pass 2nd parameter as object {header: 1}
-     console.log(this.data);
+      this.data = XLSX.utils.sheet_to_json(ws);
+      console.log("a7aaaaaaaa",this.data);
+      // const result = this.data.filter(p => p['Branch Code']=='11');
+      //  console.log("result",result);
+
     };
- }
-
-getheaders(ws:any){
-
-        // getting all rows
-let secRow=  JSON.stringify((XLSX.utils.sheet_to_json(ws, { header: 2}))[0] ); 
-secRow=secRow.replace(/"/g, "").replace(/}/g,'');
-// Fetch the first row
-let headersArr= secRow.split(":") ;
-this.tableHeaders.push({name:"Daily Sales and Stock for AL ANDALOUS Sales and Stock",title:"Date"});
-
-for (let i = 1; i < headersArr.length; i++) {
-  const element = headersArr[i];
-  let header=element.split(",");
-  debugger;
-  
-  if (header[0]==="Date") {
-    header[0]="SupCode";
   }
-    this.tableHeaders.push({name:header[1],title:header[0]});
-}
 
-
-
-}
+  get_header_row(ws: any) {
+    var range = XLSX.utils.decode_range(ws['!ref']);
+    var C, R = range.s.r;
+    /* walk every column in the range */
+    for (C = range.s.c; C <= range.e.c; ++C) {
+      var cell = ws[XLSX.utils.encode_cell({ c: C, r: R })]
+      /* find the cell in the first row */
+      var hdr = "UNKNOWN " + C; // <-- replace with your desired default 
+      if (cell && cell.t)
+        hdr = XLSX.utils.format_cell(cell);
+      this.tableHeaders.push({ name: hdr, title: hdr });
+    }
+    debugger;
+    return this.tableHeaders;
+  }
 
 
 }
--- src/app/shared/smart-table/smart-table.component.ts
+++ src/app/shared/smart-table/smart-table.component.ts
@@ -8,17 +8,17 @@ import { LocalDataSource } from 'ng2-smart-table';
   styleUrls: ['./smart-table.component.css']
 })
 export class SmartTableComponent implements OnInit {
-  @Input() data:any=[];
-  @Input() headers:any[]=[];
+  @Input() data: any = [];
+  @Input() headers: any[] = [];
   source: LocalDataSource = new LocalDataSource();
 
-  settings:any = {
-    actions:false,
+  settings: any = {
+    actions: false,
     pager: {
       display: true,
-      perPage: 10,
-      pagging:true,
-   
+      perPage: 50,
+      pagging: true,
+
     },
 
 
@@ -28,35 +28,29 @@ export class SmartTableComponent implements OnInit {
 
   }
 
-  constructor() { 
-    
+  constructor() {
+
   }
 
-  ceateTableHeaders(){
+  ceateTableHeaders() {
     this.headers.forEach(column => {
-      this.settings.columns[column.name] ={ title:column.title };
-
+      this.settings.columns[column.name] = { title: column.title };
     });
-    this.data=this.data.splice(1);
     this.source.load(this.data);
-
+    console.log("thisData", this.data);
   }
 
   ngOnInit(): void {
     this.ceateTableHeaders();
-
   }
 
   onSearch(query: string = '') {
-let filterArr:any=[];
+    let filterArr: any = [];
     this.headers.forEach(columnname => {
-      filterArr.push({field:columnname,search:query});
+      filterArr.push({ field: columnname, search: query });
 
     });
-    this.source.setFilter(filterArr,false); 
-    // second parameter specifying whether to perform 'AND' or 'OR' search 
-    // (meaning all columns should contain search query or at least one)
-    // 'AND' by default, so changing to 'OR' by setting false here
+    this.source.setFilter(filterArr, false);
   }
 
 }
